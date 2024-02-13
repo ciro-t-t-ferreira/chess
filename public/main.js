d3637da5414564ccc5a33ca2f0e235e15c012c34
ciro-t-t-ferreira <ferreira.cirott@gmail.com>
2024-02-13 17:26:48 -0300

    calculates kings moves on empty board

--- public/main.js
+++ public/main.js
@@ -1,31 +1,130 @@
 "use strict";
+/*
+    Refat:
+        -it must be a simpler way of describing the king's moves
+*/
 var Colors;
 (function (Colors) {
     Colors[Colors["white"] = 0] = "white";
     Colors[Colors["black"] = 1] = "black";
 })(Colors || (Colors = {}));
-var Kinds;
-(function (Kinds) {
-    Kinds[Kinds["pawn"] = 0] = "pawn";
-    Kinds[Kinds["knight"] = 1] = "knight";
-    Kinds[Kinds["bishop"] = 2] = "bishop";
-    Kinds[Kinds["rook"] = 3] = "rook";
-    Kinds[Kinds["queen"] = 4] = "queen";
-    Kinds[Kinds["king"] = 5] = "king";
-})(Kinds || (Kinds = {}));
 class Squares {
     constructor(column, row) {
         this.column = column;
         this.row = row;
         this.piece = null;
-        console.log(this.column.toString() + this.row.toString());
+    }
+    createPiece(piece) {
+        this.piece = piece;
+    }
+    destructPiece() {
+        this.piece = null;
     }
 }
 class Piece {
-    constructor(squareId, color, kind) {
-        this.squareId = squareId;
+    constructor(color) {
         this.color = color;
-        this.kind = kind;
+    }
+}
+/*
+class Pawn extends Piece{
+    constructor(color: Colors){
+    super(color);}
+    
+    legalMoves(square:Squares): [number, number][] {
+        let legalSquares: [number, number][];
+        return legalSquares;
+    }
+}
+
+class Knight extends Piece{
+    constructor(color: Colors){
+    super(color);}
+    
+    legalMoves(square:Squares): [number, number][] {
+        let legalSquares: Squares[] = [];
+        return legalSquares;
+    }
+}
+
+class Bishop extends Piece{
+    constructor(color: Colors){
+    super(color);}
+    
+    legalMoves(square:Squares): Squares[] {
+        let legalSquares: Squares[] = [];
+        return legalSquares;
+    }
+}
+
+class Rook extends Piece{
+    constructor(color: Colors){
+    super(color);}
+    
+    legalMoves(square:Squares): Squares[] {
+        let legalSquares: Squares[] = [];
+        return legalSquares;
+    }
+}
+
+class Queen extends Piece{
+    constructor(color: Colors){
+    super(color);}
+    
+    legalMoves(square:Squares): Squares[] {
+        let legalSquares: Squares[] = [];
+        return legalSquares;
+    }
+}
+
+*/
+class King extends Piece {
+    constructor(color) {
+        super(color);
+    }
+    static legalMoves(square) {
+        let legalSquares = [];
+        let column = square.column;
+        let row = square.row;
+        let possibleColumn;
+        let possibleRow;
+        possibleColumn = column + 1;
+        possibleRow = row;
+        isInsideBoard(possibleColumn, possibleRow) ? legalSquares.push([possibleColumn, possibleRow]) : undefined;
+        possibleColumn = column - 1;
+        possibleRow = row;
+        isInsideBoard(possibleColumn, possibleRow) ? legalSquares.push([possibleColumn, possibleRow]) : undefined;
+        possibleColumn = column;
+        possibleRow = row + 1;
+        isInsideBoard(possibleColumn, possibleRow) ? legalSquares.push([possibleColumn, possibleRow]) : undefined;
+        possibleColumn = column;
+        possibleRow = row - 1;
+        isInsideBoard(possibleColumn, possibleRow) ? legalSquares.push([possibleColumn, possibleRow]) : undefined;
+        possibleColumn = column + 1;
+        possibleRow = row + 1;
+        isInsideBoard(possibleColumn, possibleRow) ? legalSquares.push([possibleColumn, possibleRow]) : undefined;
+        possibleColumn = column - 1;
+        possibleRow = row - 1;
+        isInsideBoard(possibleColumn, possibleRow) ? legalSquares.push([possibleColumn, possibleRow]) : undefined;
+        possibleColumn = column + 1;
+        possibleRow = row - 1;
+        isInsideBoard(possibleColumn, possibleRow) ? legalSquares.push([possibleColumn, possibleRow]) : undefined;
+        possibleColumn = column - 1;
+        possibleRow = row + 1;
+        isInsideBoard(possibleColumn, possibleRow) ? legalSquares.push([possibleColumn, possibleRow]) : undefined;
+        console.log(legalSquares);
+        return legalSquares;
+    }
+    legalMoves(square) {
+        return King.legalMoves(square);
+    }
+}
+function isInsideBoard(column, row) {
+    if ((0 <= column && column <= 7) && (0 <= row && row <= 7)) {
+        return true;
+    }
+    else {
+        return false;
     }
 }
 //Responsible for verifying if there is a Piece in the square to be moved,
@@ -39,14 +138,6 @@ function movePiece(piece, fromSquare, toSquare) {
         throw new Error("Specified piece not found in Square");
     }
 }
-/*let squareA1, squareA2, squareA3, squareA4, squareA5, squareA6, squareA7, squareA8: Squares;
-let squareB1, squareB2, squareB3, squareB4, squareB5, squareB6, squareB7, squareB8: Squares;
-let squareC1, squareC2, squareC3, squareC4, squareC5, squareC6, squareC7, squareC8: Squares;
-let squareD1, squareD2, squareD3, squareD4, squareD5, squareD6, squareD7, squareD8: Squares;
-let squareE1, squareE2, squareE3, squareE4, squareE5, squareE6, squareE7, squareE8: Squares;
-let squareF1, squareF2, squareF3, squareF4, squareF5, squareF6, squareF7, squareF8: Squares;
-let squareG1, squareG2, squareG3, squareG4, squareG5, squareG6, squareG7, squareG8: Squares;
-let squareH1, squareH2, squareH3, squareH4, squareH5, squareH6, squareH7, squareH8: Squares;*/
 let tableState = [];
 initializeEmptyBoard();
 function initializeEmptyBoard() {
@@ -57,94 +148,17 @@ function initializeEmptyBoard() {
         }
     }
 }
-/*function initializeEmptyBoard(){
-    squareA1 = new Squares(Columns.A, Rows.r1);
-    squareA2 = new Squares(Columns.A, Rows.r2);
-    squareA3 = new Squares(Columns.A, Rows.r3);
-    squareA4 = new Squares(Columns.A, Rows.r4);
-    squareA5 = new Squares(Columns.A, Rows.r5);
-    squareA6 = new Squares(Columns.A, Rows.r6);
-    squareA7 = new Squares(Columns.A, Rows.r7);
-    squareA8 = new Squares(Columns.A, Rows.r8);
-
-    squareB1 = new Squares(Columns.B, Rows.r1);
-    squareB2 = new Squares(Columns.B, Rows.r2);
-    squareB3 = new Squares(Columns.B, Rows.r3);
-    squareB4 = new Squares(Columns.B, Rows.r4);
-    squareB5 = new Squares(Columns.B, Rows.r5);
-    squareB6 = new Squares(Columns.B, Rows.r6);
-    squareB7 = new Squares(Columns.B, Rows.r7);
-    squareB8 = new Squares(Columns.B, Rows.r8);
-
-    squareC1 = new Squares(Columns.C, Rows.r1);
-    squareC2 = new Squares(Columns.C, Rows.r2);
-    squareC3 = new Squares(Columns.C, Rows.r3);
-    squareC4 = new Squares(Columns.C, Rows.r4);
-    squareC5 = new Squares(Columns.C, Rows.r5);
-    squareC6 = new Squares(Columns.C, Rows.r6);
-    squareC7 = new Squares(Columns.C, Rows.r7);
-    squareC8 = new Squares(Columns.C, Rows.r8);
-
-    squareD1 = new Squares(Columns.D, Rows.r1);
-    squareD2 = new Squares(Columns.D, Rows.r2);
-    squareD3 = new Squares(Columns.D, Rows.r3);
-    squareD4 = new Squares(Columns.D, Rows.r4);
-    squareD5 = new Squares(Columns.D, Rows.r5);
-    squareD6 = new Squares(Columns.D, Rows.r6);
-    squareD7 = new Squares(Columns.D, Rows.r7);
-    squareD8 = new Squares(Columns.D, Rows.r8);
-
-    squareE1 = new Squares(Columns.E, Rows.r1);
-    squareE2 = new Squares(Columns.E, Rows.r2);
-    squareE3 = new Squares(Columns.E, Rows.r3);
-    squareE4 = new Squares(Columns.E, Rows.r4);
-    squareE5 = new Squares(Columns.E, Rows.r5);
-    squareE6 = new Squares(Columns.E, Rows.r6);
-    squareE7 = new Squares(Columns.E, Rows.r7);
-    squareE8 = new Squares(Columns.E, Rows.r8);
-
-    squareF1 = new Squares(Columns.F, Rows.r1);
-    squareF2 = new Squares(Columns.F, Rows.r2);
-    squareF3 = new Squares(Columns.F, Rows.r3);
-    squareF4 = new Squares(Columns.F, Rows.r4);
-    squareF5 = new Squares(Columns.F, Rows.r5);
-    squareF6 = new Squares(Columns.F, Rows.r6);
-    squareF7 = new Squares(Columns.F, Rows.r7);
-    squareF8 = new Squares(Columns.F, Rows.r8);
-
-    squareG1 = new Squares(Columns.G, Rows.r1);
-    squareG2 = new Squares(Columns.G, Rows.r2);
-    squareG3 = new Squares(Columns.G, Rows.r3);
-    squareG4 = new Squares(Columns.G, Rows.r4);
-    squareG5 = new Squares(Columns.G, Rows.r5);
-    squareG6 = new Squares(Columns.G, Rows.r6);
-    squareG7 = new Squares(Columns.G, Rows.r7);
-    squareG8 = new Squares(Columns.G, Rows.r8);
-
-    squareH1 = new Squares(Columns.H, Rows.r1);
-    squareH2 = new Squares(Columns.H, Rows.r2);
-    squareH3 = new Squares(Columns.H, Rows.r3);
-    squareH4 = new Squares(Columns.H, Rows.r4);
-    squareH5 = new Squares(Columns.H, Rows.r5);
-    squareH6 = new Squares(Columns.H, Rows.r6);
-    squareH7 = new Squares(Columns.H, Rows.r7);
-    squareH8 = new Squares(Columns.H, Rows.r8);
-
+initializePieces();
+function initializePieces() {
+    let whiteKing = new King(Colors.white);
+    tableState[3][3].createPiece(whiteKing);
+    King.legalMoves(tableState[3][3]);
 }
+/*
+    Player clicks square
+    HTML square verifies pieces present in the square
+        by checking tableState[column][row]
+    Model obtains the square and runs logic by the
+        Piece in the abstract
 
-initializeEmptyBoard();*/
-/*function putPieceInSquare(squareId:Squares, color:Colors, kind:Kinds){
-
-    let square: HTMLElement|null = document.getElementById(squareId.toString())
-
-    let imageSource: string = createImageURL(color, kind);
-    console.log(imageSource);
-    let piece: HTMLElement|null = document.createElement('img');
-    piece.setAttribute('src', imageSource);
-    square?.appendChild(piece);
-}
-
-function createImageURL(color: Colors, kind: Kinds): string{
-    let URL: string = 'img/' + color.toString() + '-' + kind.toString() + '.png';
-    return URL;
-}*/ 
+*/
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,11 @@
+/*
+    Refat: 
+        -it must be a simpler way of describing the king's moves
+*/
+
 enum Colors{
     'white',
     'black'
-}
-
-enum Kinds{
-    'pawn',
-    'knight',
-    'bishop',
-    'rook',
-    'queen',
-    'king'
 }
 
 class Squares{
@@ -21,19 +17,149 @@ class Squares{
         this.column = column;
         this.row = row;
         this.piece = null;
-        console.log(this.column.toString() + this.row.toString())
+    }
+
+    createPiece(piece: Piece){
+        this.piece = piece;
+    }
+
+    destructPiece(){
+        this.piece = null;
     }
 }
 
-class Piece{
-    public squareId: Squares;
-    public color: Colors;
-    public kind: Kinds;
+abstract class Piece{
+    public color:Colors;
 
-    constructor(squareId:Squares, color:Colors, kind:Kinds ){
-        this.squareId = squareId;
+    constructor(color: Colors){
         this.color = color;
-        this.kind = kind;        
+    }
+
+    abstract legalMoves(square:Squares): [number, number][];
+}
+
+/*
+class Pawn extends Piece{
+    constructor(color: Colors){
+    super(color);}
+    
+    legalMoves(square:Squares): [number, number][] {
+        let legalSquares: [number, number][];
+        return legalSquares;
+    }    
+}
+
+class Knight extends Piece{
+    constructor(color: Colors){
+    super(color);}
+    
+    legalMoves(square:Squares): [number, number][] {
+        let legalSquares: Squares[] = [];
+        return legalSquares;
+    }    
+}
+
+class Bishop extends Piece{
+    constructor(color: Colors){
+    super(color);}
+    
+    legalMoves(square:Squares): Squares[] {
+        let legalSquares: Squares[] = [];
+        return legalSquares;
+    }    
+}
+
+class Rook extends Piece{
+    constructor(color: Colors){
+    super(color);}
+    
+    legalMoves(square:Squares): Squares[] {
+        let legalSquares: Squares[] = [];
+        return legalSquares;
+    }    
+}
+
+class Queen extends Piece{
+    constructor(color: Colors){
+    super(color);}
+    
+    legalMoves(square:Squares): Squares[] {
+        let legalSquares: Squares[] = [];
+        return legalSquares;
+    }    
+}
+
+*/
+
+class King extends Piece{
+    constructor(color: Colors){
+    super(color);}
+    
+    static legalMoves(square:Squares): [number , number][] {
+        let legalSquares: [number , number][] = [] ;
+        let column = square.column;
+        let row = square.row;
+
+        let possibleColumn: number;
+        let possibleRow:number;
+
+        possibleColumn = column + 1;
+        possibleRow = row;
+        isInsideBoard(possibleColumn, possibleRow)? legalSquares.push(
+            [possibleColumn, possibleRow]): undefined;  
+        
+        possibleColumn = column - 1;
+        possibleRow = row;
+        isInsideBoard(possibleColumn, possibleRow)? legalSquares.push(
+            [possibleColumn, possibleRow]): undefined;
+        
+        possibleColumn = column;
+        possibleRow = row + 1;
+        isInsideBoard(possibleColumn, possibleRow)? legalSquares.push(
+            [possibleColumn, possibleRow]): undefined;
+        
+        possibleColumn = column;
+        possibleRow = row -1;
+        isInsideBoard(possibleColumn, possibleRow)? legalSquares.push(
+            [possibleColumn, possibleRow]): undefined;
+
+        possibleColumn = column + 1;
+        possibleRow = row + 1;
+        isInsideBoard(possibleColumn, possibleRow)? legalSquares.push(
+            [possibleColumn, possibleRow]): undefined;
+
+        possibleColumn = column - 1;
+        possibleRow = row - 1;
+        isInsideBoard(possibleColumn, possibleRow)? legalSquares.push(
+            [possibleColumn, possibleRow]): undefined;
+
+        possibleColumn = column + 1;
+        possibleRow = row - 1;
+        isInsideBoard(possibleColumn, possibleRow)? legalSquares.push(
+            [possibleColumn, possibleRow]): undefined;
+
+        possibleColumn = column - 1;
+        possibleRow = row + 1;
+        isInsideBoard(possibleColumn, possibleRow)? legalSquares.push(
+            [possibleColumn, possibleRow]): undefined;            
+            
+        console.log(legalSquares);
+
+        return legalSquares;
+    }
+    
+    legalMoves(square:Squares):[number, number][] {
+         return King.legalMoves(square);
+    }
+    
+}
+
+function isInsideBoard(column: number, row: number): boolean{
+    if ((0 <= column && column <=7) && (0 <= row && row <=7)){
+        return true;
+    }
+    else{ 
+        return false
     }
 }
 
@@ -51,14 +177,6 @@ function movePiece(piece: Piece, fromSquare: Squares, toSquare: Squares){
 
 }
 
-/*let squareA1, squareA2, squareA3, squareA4, squareA5, squareA6, squareA7, squareA8: Squares;
-let squareB1, squareB2, squareB3, squareB4, squareB5, squareB6, squareB7, squareB8: Squares;
-let squareC1, squareC2, squareC3, squareC4, squareC5, squareC6, squareC7, squareC8: Squares;
-let squareD1, squareD2, squareD3, squareD4, squareD5, squareD6, squareD7, squareD8: Squares;
-let squareE1, squareE2, squareE3, squareE4, squareE5, squareE6, squareE7, squareE8: Squares;
-let squareF1, squareF2, squareF3, squareF4, squareF5, squareF6, squareF7, squareF8: Squares;
-let squareG1, squareG2, squareG3, squareG4, squareG5, squareG6, squareG7, squareG8: Squares;
-let squareH1, squareH2, squareH3, squareH4, squareH5, squareH6, squareH7, squareH8: Squares;*/
 
 let tableState: Array<Array<Squares>> = []
 
@@ -77,95 +195,20 @@ function initializeEmptyBoard(){
 
 }
 
-/*function initializeEmptyBoard(){
-    squareA1 = new Squares(Columns.A, Rows.r1);
-    squareA2 = new Squares(Columns.A, Rows.r2);
-    squareA3 = new Squares(Columns.A, Rows.r3);
-    squareA4 = new Squares(Columns.A, Rows.r4);
-    squareA5 = new Squares(Columns.A, Rows.r5);
-    squareA6 = new Squares(Columns.A, Rows.r6);
-    squareA7 = new Squares(Columns.A, Rows.r7);
-    squareA8 = new Squares(Columns.A, Rows.r8);
+initializePieces();
 
-    squareB1 = new Squares(Columns.B, Rows.r1);
-    squareB2 = new Squares(Columns.B, Rows.r2);
-    squareB3 = new Squares(Columns.B, Rows.r3);
-    squareB4 = new Squares(Columns.B, Rows.r4);
-    squareB5 = new Squares(Columns.B, Rows.r5);
-    squareB6 = new Squares(Columns.B, Rows.r6);
-    squareB7 = new Squares(Columns.B, Rows.r7);
-    squareB8 = new Squares(Columns.B, Rows.r8);
-
-    squareC1 = new Squares(Columns.C, Rows.r1);
-    squareC2 = new Squares(Columns.C, Rows.r2);
-    squareC3 = new Squares(Columns.C, Rows.r3);
-    squareC4 = new Squares(Columns.C, Rows.r4);
-    squareC5 = new Squares(Columns.C, Rows.r5);
-    squareC6 = new Squares(Columns.C, Rows.r6);
-    squareC7 = new Squares(Columns.C, Rows.r7);
-    squareC8 = new Squares(Columns.C, Rows.r8);
-
-    squareD1 = new Squares(Columns.D, Rows.r1);
-    squareD2 = new Squares(Columns.D, Rows.r2);
-    squareD3 = new Squares(Columns.D, Rows.r3);
-    squareD4 = new Squares(Columns.D, Rows.r4);
-    squareD5 = new Squares(Columns.D, Rows.r5);
-    squareD6 = new Squares(Columns.D, Rows.r6);
-    squareD7 = new Squares(Columns.D, Rows.r7);
-    squareD8 = new Squares(Columns.D, Rows.r8);
-
-    squareE1 = new Squares(Columns.E, Rows.r1);
-    squareE2 = new Squares(Columns.E, Rows.r2);
-    squareE3 = new Squares(Columns.E, Rows.r3);
-    squareE4 = new Squares(Columns.E, Rows.r4);
-    squareE5 = new Squares(Columns.E, Rows.r5);
-    squareE6 = new Squares(Columns.E, Rows.r6);
-    squareE7 = new Squares(Columns.E, Rows.r7);
-    squareE8 = new Squares(Columns.E, Rows.r8);
-
-    squareF1 = new Squares(Columns.F, Rows.r1);
-    squareF2 = new Squares(Columns.F, Rows.r2);
-    squareF3 = new Squares(Columns.F, Rows.r3);
-    squareF4 = new Squares(Columns.F, Rows.r4);
-    squareF5 = new Squares(Columns.F, Rows.r5);
-    squareF6 = new Squares(Columns.F, Rows.r6);
-    squareF7 = new Squares(Columns.F, Rows.r7);
-    squareF8 = new Squares(Columns.F, Rows.r8);
-
-    squareG1 = new Squares(Columns.G, Rows.r1);
-    squareG2 = new Squares(Columns.G, Rows.r2);
-    squareG3 = new Squares(Columns.G, Rows.r3);
-    squareG4 = new Squares(Columns.G, Rows.r4);
-    squareG5 = new Squares(Columns.G, Rows.r5);
-    squareG6 = new Squares(Columns.G, Rows.r6);
-    squareG7 = new Squares(Columns.G, Rows.r7);
-    squareG8 = new Squares(Columns.G, Rows.r8);
-
-    squareH1 = new Squares(Columns.H, Rows.r1);
-    squareH2 = new Squares(Columns.H, Rows.r2);
-    squareH3 = new Squares(Columns.H, Rows.r3);
-    squareH4 = new Squares(Columns.H, Rows.r4);
-    squareH5 = new Squares(Columns.H, Rows.r5);
-    squareH6 = new Squares(Columns.H, Rows.r6);
-    squareH7 = new Squares(Columns.H, Rows.r7);
-    squareH8 = new Squares(Columns.H, Rows.r8);
-
+function initializePieces(){
+    
+    let whiteKing = new King(Colors.white);
+    tableState[3][3].createPiece(whiteKing);
+    King.legalMoves(tableState[3][3]);
 }
 
-initializeEmptyBoard();*/
+/*
+    Player clicks square
+    HTML square verifies pieces present in the square
+        by checking tableState[column][row]
+    Model obtains the square and runs logic by the 
+        Piece in the abstract
 
-/*function putPieceInSquare(squareId:Squares, color:Colors, kind:Kinds){
-
-    let square: HTMLElement|null = document.getElementById(squareId.toString())
-
-    let imageSource: string = createImageURL(color, kind);
-    console.log(imageSource);
-    let piece: HTMLElement|null = document.createElement('img');
-    piece.setAttribute('src', imageSource);
-    square?.appendChild(piece);
-}
-
-function createImageURL(color: Colors, kind: Kinds): string{
-    let URL: string = 'img/' + color.toString() + '-' + kind.toString() + '.png';
-    return URL;
-}*/
+*/
